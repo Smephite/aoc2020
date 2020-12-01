efd05b7da418490f6f14dfe29dc8d7c89a4d301c
Kai Berszin <mail@kaibersz.in>
2020-12-02 00:26:57 +0100

    Added improved Day 1 Part 1

--- src/day1.rs
+++ src/day1.rs
@@ -9,19 +9,35 @@ fn parse_input_day1(input: &str) -> Result<Vec<i32>, ParseIntError> {
 #[aoc(day1, part1)]
 fn part1(expenses: &[i32]) -> i32 {
     for expense in expenses {
-        if expenses.contains(&(2020 - expense)) {
-            return expense*(2020-expense);
+        let diff = 2020-expense;
+        if expenses.contains(&diff) {
+            return expense*diff;
         }
     }
+    -1
+}
+#[aoc(day1, part1, array)]
+fn part1_arr(expenses: &[i32]) -> i32 {
+    let mut array : [bool; 2020] = [false; 2020];
+
+    for expense in expenses {
+        if array[*expense as usize] {
+            return expense*(2020 - expense);
+        }
+        array[(2020-expense) as usize] = true;
+    }
+
     -1
 }
 
 #[aoc(day1, part2)]
 fn part2(expenses: &[i32]) -> i32 {
-    for expense1 in expenses {
-        for expense2 in expenses {
-            if expenses.contains(&(2020 - expense1 - expense2)) {
-                return expense1*expense2*(2020 - expense1 - expense2);
+    for (i, expense1) in expenses.into_iter().enumerate() {
+        let diff1 = 2020 - expense1;
+        for expense2 in &expenses[i + 1 ..].to_vec() {
+            let diff2 = diff1 - expense2;
+            if expenses.contains(&diff2) {
+                return expense1*expense2*diff2;
             }
         }
     }
@@ -36,6 +52,10 @@ mod tests {
     #[test]
     fn part1_example() {
         assert_eq!(part1(&[1721, 979, 366, 299, 675, 1456]), 514579);
+    }
+    #[test]
+    fn part1_example_array() {
+        assert_eq!(part1_arr(&[1721, 979, 366, 299, 675, 1456]), 514579);
     }
     #[test]
     fn part2_example() {

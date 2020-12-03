08825f1e6d9b2ef9ec59bf0b10f4e711b0b01daa
Kai Berszin <mail@kaibersz.in>
2020-12-03 09:16:00 +0100

    Added another solution and generator for day 03

--- src/day3.rs
+++ src/day3.rs
@@ -15,6 +15,15 @@ fn aoc_generator(input : &str) -> Map{
     map
 
 }
+// this one is fancier but slower
+//#[aoc_generator(day3)]
+fn aoc_generator_2(input : &str) -> Map {
+    let data : Vec<Vec<bool>> = input.lines().map(|l| l.chars().map(|c| c == '#').collect()).collect();
+    let mut map = Map::create(&data.len(), &data[0].len());
+    map.data = data;
+
+    map
+}
 
 #[derive(Debug)]
 struct Map {
@@ -48,6 +57,20 @@ impl Map {
         assert_eq!(*y < self.height, true);
         self.data[*y][x % self.width]
     }
+
+    fn count_trees(&self, x_inc: usize, y_inc: usize) -> usize {
+        let mut x = 0;
+
+        (0..self.height).step_by(y_inc)
+        .filter(|&r| {
+            let x_org = x;
+            x += x_inc;
+            if x >= self.width {
+                x -= self.width;
+            }
+            self.data[r][x_org]
+        }).count()
+    }
 }
 
 
@@ -66,6 +89,11 @@ fn part1(map: &Map) -> usize {
         y += 1;
     }
     trees
+}
+
+#[aoc(day3, part1, no_mod)]
+fn part1_no_mod(map: &Map) -> usize {
+    map.count_trees(3, 1)
 }
 
 #[aoc(day3, part2)]
@@ -93,6 +121,13 @@ fn part2(map: &Map) -> usize {
     response
 }
 
+#[aoc(day3, part2, no_mod)]
+fn part2_no_mod(map: &Map) -> usize {
+    let slopes = [(1, 1), (3,1), (5,1), (7,1), (1,2)];
+
+    slopes.iter().map(|(x, y)| map.count_trees(*x, *y)).product()
+}
+
 
 
 
@@ -105,12 +140,14 @@ mod tests {
     fn part1_test() {
         let test_data = "..##.......\n#...#...#..\n.#....#..#.\n..#.#...#.#\n.#...##..#.\n..#.##.....\n.#.#.#....#\n.#........#\n#.##...#...\n#...##....#\n.#..#...#.#";
         assert_eq!(part1(&aoc_generator(test_data)), 7);
+        assert_eq!(part1_no_mod(&aoc_generator(test_data)), 7);
     }
 
     #[test]
     fn part2_test() {
         let test_data = "..##.......\n#...#...#..\n.#....#..#.\n..#.#...#.#\n.#...##..#.\n..#.##.....\n.#.#.#....#\n.#........#\n#.##...#...\n#...##....#\n.#..#...#.#";
         assert_eq!(part2(&aoc_generator(test_data)), 336);
+        assert_eq!(part2_no_mod(&aoc_generator(test_data)), 336);
     }
 
 }
